ee09e3c00b96c542549e83406b5047db696dcda5
Chrstjan <ps5christian@hotmail.com>
2024-03-04 10:35:10 +0100

    Hamburger Menu

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,7 @@
 // write cool JS hwere!!
+const hamburgerBtn = document.getElementById("hamburger");
+const navList = document.querySelector(".main-list");
+
+hamburgerBtn.addEventListener("click", () => {
+    navList.classList.toggle("show-nav");
+})
